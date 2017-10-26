36d7c133afa4cc6175ea6a814ede204d3ef660f2
RileyBee <rileybabao@gmail.com>
2017-10-26 11:36:29 -0400

    Add files via upload
    Added class library

---
+++ DungeonCrawl_NoyesPrivette_3/Classes/Battle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawl_NoyesPrivette_3
+{
+    public class Battle
+    {
+        private Player _battlePlayer;
+        private Enemy _battleEnemy;
+
+        public Battle()
+        {
+            _battlePlayer = null;
+            _battleEnemy = null;       }
+
+        public Battle(Player pl, Enemy en)
+        {
+            _battlePlayer = pl;
+            _battleEnemy = en;
+        }
+
+        public Player battlePlayer
+        {
+            get { return _battlePlayer; }
+            set { _battlePlayer = value; }
+        }
+
+        public Enemy battleEnemy
+        {
+            get { return _battleEnemy; }
+            set { _battleEnemy = value; }
+        }
+    }
+}
